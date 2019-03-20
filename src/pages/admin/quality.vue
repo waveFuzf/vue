2aff3e4046018024d61a67cef61a7a6fe8e70b40
傅郑锋 <fuzhengfeng@zhuojianchina.com>
2019-03-20 10:17:36 +0800

    no message

--- src/pages/admin/quality.vue
+++ src/pages/admin/quality.vue
@@ -3,27 +3,31 @@
         <el-row style="border-left:4px solid #409EFF;">
             <span style="font-size:20px;color:#425b77;margin:20px;">搜索条件</span><br>
         <el-row style="margin-top:20px;margin-left:30px;">
-            <el-date-picker v-model="form.beginTime" type="date" placeholder="起始日期">
+            <el-date-picker v-model="selectForm.beginTime" type="date" placeholder="起始日期" value-format=" yyyy-MM-dd" format="yyyy-MM-dd">
             </el-date-picker>
-            <el-date-picker v-model="form.endTime" type="date" placeholder="结束日期">
+            <el-date-picker v-model="selectForm.endTime" type="date" placeholder="结束日期" value-format=" yyyy-MM-dd" format="yyyy-MM-dd">
             </el-date-picker>
         </el-row>
         <el-row style="margin-top:20px;margin-left:30px;">
-            <el-tabs v-model="form.activeName" style="float:left;width:240px;">
+            <el-tabs v-model="selectForm.activeName" style="float:left;width:240px;">
                 <el-tab-pane label="切片质量" name="1"></el-tab-pane>
                 <el-tab-pane label="初步诊断质量" name="2"></el-tab-pane>
             </el-tabs>
-            <el-radio v-model="form.radio" label="1" style="line-height:40px;">按医院</el-radio>
-            <el-radio v-model="form.radio" label="2">按部位</el-radio>
+            <el-radio v-model="selectForm.radio" label="1" style="line-height:40px;">按医院</el-radio>
+            <el-radio v-model="selectForm.radio" label="2">按部位</el-radio>
         </el-row>
+        </el-row>
+        <el-row style="font-size:14px;color:#425b77;margin-top:20px;line-height:28px;">
+            <span>共有 <span style="color:#409EFF;">{{res.length}} </span>项</span>
+            <el-button style="float:right;" size="mini">导出Excel</el-button>
         </el-row>
         <el-table
           v-loading="loading"
           :data="res"
           stripe
-          style="border:1px solid #cccdce;width:100%;height:290px;margin-top:20px;"
+          style="border:1px solid #cccdce;width:880px;height:290px;margin-top:20px;"
         >
-          <el-table-column prop="name" :label="form.radio=='1'?'医院名称':'部位'" width="130"></el-table-column>
+          <el-table-column prop="name" :label="selectForm.radio=='1'?'医院名称':'部位'" width="130"></el-table-column>
           <el-table-column prop="one" label="质量不合格" width="120"></el-table-column>
           <el-table-column prop="two" label="质量基本合格" width="120"></el-table-column>
           <el-table-column prop="three" label="质量合格" width="120"></el-table-column>
@@ -35,7 +39,6 @@
               </template>
           </el-table-column>
         </el-table> 
-        {{form}}     
 
 
     </div>
@@ -49,13 +52,22 @@ export default {
        
     },
     watch:{
-        form:function(){
-            this.selectQualityInfo();
+        selectForm:{
+            handler(){
+                if(!!this.selectForm.beginTime==!!this.selectForm.endTime){
+                                  this.selectQualityInfo();
+                }
+            },
+            deep:true,
+            immediate:true,
+        },
+        count:function(){
+            console.log(this.count);
         }
     },
     data() {
         return{
-           form:{
+           selectForm:{
                beginTime:null,
                endTime:null,
                activeName:"1",
@@ -76,7 +88,7 @@ export default {
          },
          selectQualityInfo(){
              this.loading=true;
-             axion.selectQualityInfo(this.form).then(res=>{
+             axion.selectQualityInfo(this.selectForm).then(res=>{
                  this.res=res.data.data;
                  this.count=parseInt(res.data.message);
                  this.loading=false;
